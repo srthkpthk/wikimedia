caf74d5808d7400ec534f0718ed3e7e182b6e966
Sarthak Pathak <sarthak.pathak@grappus.com>
2024-06-21 19:59:16 +0530

    add

---
+++ wikimedia_consumer/src/main/java/pthk/srthk/wikimedia_consumer/KafkaConfig.java
@@ -0,0 +1,10 @@
+package pthk.srthk.wikimedia_consumer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+}
